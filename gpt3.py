e5434cb033aff7c73f0c0da004286e8923058056
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-28 23:53:44 +0200

    Update gpt3.py

--- gpt3.py
+++ gpt3.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-openai.api_key = "Insert here your Open AI API secret key"
+openai.api_key = SECRET_KEY
 response = openai.Completion.create(engine="davinci", prompt="I expect", max_tokens=5)
 
 st.text(response["choices"][0]["text"])
