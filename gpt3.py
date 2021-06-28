a9bfa145e80710869bc8a2d37cc0efe3faac6743
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-29 00:08:59 +0200

    Update gpt3.py

--- gpt3.py
+++ gpt3.py
@@ -1,14 +1,13 @@
 import openai
 import streamlit as st
-
 openai.api_key = st.secrets["SECRET_KEY"]
 response = openai.Completion.create(engine="davinci", prompt="I expect", max_tokens=5)
 
-st.text(response["choices"][0]["text"])
-
-
-
 st.title('GPT-3')
+
 st.text('This GPT-3 model is the Davinci-engine')
 prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
+st.text('Remaining phrase:')
+st.text(response["choices"][0]["text"])
+
 
